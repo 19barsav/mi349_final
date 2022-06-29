401a95c770f7c6aa0bbc6ab3d4577a85b4c1ded2
19barsav <barne329@msu.edu>
2022-06-29 17:52:00 -0400

    i think she's done, need to validate!

--- sidebar.js
+++ sidebar.js
@@ -2,7 +2,7 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
-    document.getElementById("side_open").style.display = none;
+    document.getElementById("side_open").style.display = null;
   }
   
   /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
